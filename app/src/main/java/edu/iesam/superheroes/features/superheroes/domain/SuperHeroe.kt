4354d920d6f4598677601092217f379608c9bde9
FDI <isanchod@gmail.com>
2025-10-01 20:29:51 +0200

    Crear las clases iniciales

---
+++ app/src/main/java/edu/iesam/superheroes/features/superheroes/domain/SuperHeroe.kt
@@ -0,0 +1,4 @@
+package edu.iesam.superheroes.features.superheroes.domain
+
+class SuperHeroe(val id: String, val name: String, val slug: String, val urlImage: String) {
+}
--- app/src/main/java/edu/iesam/superheroes/features/superheroes/presentation/MainActivity.kt
+++ app/src/main/java/edu/iesam/superheroes/features/superheroes/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package edu.iesam.superheroes
+package edu.iesam.superheroes.features.superheroes.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import edu.iesam.superheroes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,9 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+        //llamada
+        //val supeheroes = //
         }
     }
 }
