2ba802a2480328205949f9b9840d1603b59e937a
FDI <isanchod@gmail.com>
2025-10-10 15:39:52 +0200

    metodo findById de SuperHeroApiService.kt ahora está declarada como suspend

--- app/src/main/java/edu/iesam/superheroes/features/superheroes/data/remote/api/SuperHeroApiService.kt
+++ app/src/main/java/edu/iesam/superheroes/features/superheroes/data/remote/api/SuperHeroApiService.kt
@@ -11,5 +11,5 @@ interface SuperHeroApiService {
     suspend fun findAll(): Response<List<SuperHeroApiModel>>
 
     @GET("id/{superheroId}.json")
-    fun findById(@Path("superheroId") id: String): Response<SuperHeroApiModel>
+    suspend fun findById(@Path("superheroId") id: String): Response<SuperHeroApiModel>
 }
